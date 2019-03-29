35c2cf153ff8c6c621f124ee64148c1d28d6b24b
Alex Flores <alex_fv92@hotmail.com>
2019-03-29 16:43:12 -0400

    AppBarBottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      new MaterialApp(
-        home: MyApp(),
-      ));
+  runApp(new MaterialApp(
+    home: MyApp(),
+  ));
 }
+
+
 class MyApp extends StatelessWidget {
   @override
+  int _selected=0;
+  final _statefin=[Text('item 1'),Text('item 2'),Text('item 2')];
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: new Text("OnTimeApp"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap:(int index){
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_alarms),
+            title: Text('Alamrs')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.archive),
+            title: Text('Arch')
+          ),
+        ],
       ),
     );
   }
