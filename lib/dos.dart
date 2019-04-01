ac8cf6c3a0c2d3cea2ff3c9bca30f7397b517b7c
Alex Flores <alex_fv92@hotmail.com>
2019-03-31 22:24:37 -0400

    Colores

---
+++ lib/dos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+   home: SecondPage(),
+  ));
+}
+class SecondPage extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+  return new MaterialApp(
+  
+ );
+ }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Color.fromRGBO(49, 60, 72, 1),
         title: new Text("OnTimeApp"),
       ),
-      body: new Stack(
+     body: new Stack(
           children: <Widget>[
             new Container(
               decoration: new BoxDecoration(
@@ -27,29 +27,87 @@ class MyApp extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
-            )
+            ),
+            new Container(
+              color: Colors.black54,
+            ),
+            new Align(
+              alignment: Alignment(0.0,-0.4),
+              child: Container(
+                margin: const EdgeInsets.all(30.0),
+                padding: const EdgeInsets.all(10.0),
+                 decoration: new BoxDecoration(
+                   borderRadius: BorderRadius.circular(20.0),
+                   border: new Border.all(color: Colors.white)
+                 ),
+                child: Center(
+                  child: new Text(
+                  "A Donde Vamos...",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 30.0),
+                 ),
+                ),
+                height: 100.0,
+                width: 350.0,
+              ),
+            ),
+            new Align(
+              alignment: Alignment(0.0, -0.8),
+
+                  child: Stack(
+                    children: <Widget>[
+                      Icon(Icons.check,color: Colors.white,size: 100.0,)
+                    ],
+                  ),
+            ),
+
+            new Align(
+              alignment: Alignment(0.0,0.9),
+              child: Container(
+
+
+                decoration: new BoxDecoration(
+                    color: Color.fromRGBO(84, 103, 124, 1),
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: new Border.all(color: Colors.white)
+                ),
+                child: Center(
+                  child: new Text(
+                    "Nuestras Actualizaciones empezaran a funcionar desde el 22/03/2019",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 30.0),
+                  ),
+                ),
+                height: 300.0,
+              ),
+            ),
           ],
           ),
-      
-//
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap:(int index){
-        },
+          
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: const Color.fromRGBO(49, 60, 73, 1)
+        ),
+        child: new BottomNavigationBar(     
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home')
+            
+            icon: Icon(Icons.home, color: Colors.white),
+            title: new Text("Home",style: TextStyle(color: Colors.white))
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.access_alarms),
-            title: Text('Alamrs')
+            
+            icon: Icon(Icons.directions_transit, color: Colors.white),
+            title: Text('Transporte Colectivo',style: TextStyle(color: Colors.white))
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.archive),
-            title: Text('Arch')
+            icon: Icon(Icons.local_taxi, color: Colors.white),
+            title: Text('RadioTaxi',style: TextStyle(color: Colors.white))
           ),
         ],
+        fixedColor: Colors.yellowAccent,
+        
+        ), 
       ),
     );
   }
