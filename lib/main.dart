fff2b7de57c5b2bc30cb0c1ac749ec4d956de29e
Alex Flores <alex_fv92@hotmail.com>
2019-04-21 17:05:57 -0400

    top nav bar 4

--- lib/main.dart
+++ lib/main.dart
@@ -53,3 +53,69 @@ class _DashboardScreenState extends State<DashboardScreen> {
       this._page = page;
     });
   }
+ @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          widget.title,
+          style: new TextStyle(color: const Color(0xFFFFFFFF)),
+        ),
+      ),
+      body: new PageView(
+        children: [
+          new Home("Home screen"),
+          new Location("Location screen"),
+          new Friends("Friends screen"),
+        ],
+        onPageChanged: onPageChanged,
+        controller: _pageController,
+      ),
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+              // sets the background color of the `BottomNavigationBar`
+              canvasColor: const Color(0xFF167F67),
+            ), // sets the inactive color of the `BottomNavigationBar`
+        child: new BottomNavigationBar(
+          items: [
+            new BottomNavigationBarItem(
+                icon: new Icon(
+                  Icons.home,
+                  color: const Color(0xFFFFFFFF),
+                ),
+                title: new Text(
+                  "Home",
+                  style: new TextStyle(
+                    color: const Color(0xFFFFFFFF),
+                  ),
+                )),
+            new BottomNavigationBarItem(
+                icon: new Icon(
+                  Icons.location_on,
+                  color: const Color(0xFFFFFFFF),
+                ),
+                title: new Text(
+                  "Location",
+                  style: new TextStyle(
+                    color: const Color(0xFFFFFFFF),
+                  ),
+                )),
+            new BottomNavigationBarItem(
+                icon: new Icon(
+                  Icons.people,
+                  color: const Color(0xFFFFFFFF),
+                ),
+                title: new Text(
+                  "Friends",
+                  style: new TextStyle(
+                    color: const Color(0xFFFFFFFF),
+                  ),
+                ))
+          ],
+          onTap: navigationTapped,
+          currentIndex: _page,
+        ),
+      ),
+    );
+  }
+}
